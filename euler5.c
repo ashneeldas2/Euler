0d40edc7c65c87b5e97706c68ee96f519b5ebaab
ashneeldas2 <adas2@stuy.edu>
2017-09-15 13:10:57 -0400

    Solution to euler 5

--- euler5.c
+++ euler5.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 
 int main() {
-    int i = 0;
+    int i = 1;
     int j;
+    int breakable = 0;
     while (i > 0) {
 	for (j = 1; j < 21; j++) {
 	    if (i % j != 0) break;
+	    if (j == 20) {
+		printf("%d\n", i);
+		breakable = 1;
+	    }
 	}
 	i++;
+	if (breakable == 1) break;
     }
 }
